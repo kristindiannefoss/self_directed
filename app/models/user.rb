bced6b3d3596742cb4944bbffe2dd36f9278ec7b
Kris Foss <kris.foss@gmail.com>
2016-06-07 18:03:58 -0600

    swtiching gears

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,29 @@
 class User < ActiveRecord::Base
-  # def self.from_omniauth(auth_info)
-  #   where(uid: auth_info[:uid]).first_or_create do |new_user|
-  #     new_user.uid                = auth_info.uid
-  #     new_user.user_name          = auth_info.extra.raw_info.user_name
-  #     new_user.oauth_token        = auth_info.credentials.token
-  #     new_user.oauth_token_secret = auth_info.credentials.secret
-  #   end
-  # end
-  def self.create_from_omniauth(auth_hash)
-    self.create(
-                uid: auth_hash[:uid],
-                user_name: auth_hash[:info]["nickname"],
-                name: auth_hash[:info]["name"],
-                image: auth_hash[:info]["image"],
-                followers_url: auth_hash["extra"]["raw_info"]["followers_url"],
-                following_url: auth_hash["extra"]["raw_info"]["following_url"],
-                location: auth_hash["extra"]["raw_info"]["location"])
+  validates_presence_of :uid
+  validates_uniqueness_of :uid
+  validates_presence_of :nickname
+  validates_presence_of :token
+
+  def self.find_or_create_by_omniauth(auth_hash)
+    user = User.find_or_create_by_(provider: auth['provider', uid: auth['uid']])
+    user.uid: auth_hash[:uid],
+    user.user_name: auth_hash[:info]["nickname"],
+    user.name: auth_hash[:info]["name"],
+    user.image: auth_hash[:info]["image"],
+    user.followers_url: auth_hash["extra"]["raw_info"]["followers_url"],
+    user.following_url: auth_hash["extra"]["raw_info"]["following_url"],
+    user.location: auth_hash["extra"]["raw_info"]["location"],
+    user.token: auth_hash["credentials"]["token"])
+
+    user.save
+    user
+  end
+
+  def create_following_url
+    "https://github.com/" + "#{self.user_name}" + "/following"
+  end
+
+  def create_followers_url
+    "https://github.com/" + "#{self.user_name}" + "/followers"
   end
 end
