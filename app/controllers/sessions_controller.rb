551bf05fb6a233082b9a08dc72c39ab6ea3b31b1
Kris Foss <kris.foss@gmail.com>
2016-06-07 22:43:38 -0600

    switches oauth again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,19 @@
 class SessionsController < ApplicationController
 
-
   def create
-    @user = User.find_by(id: auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
-    if @user
-         session[:user_id] = @user.id
-         redirect_to user_path(current_user)
-    else
-         redirect_to root_url
-    end
+    raise request.env["omniauth.auth"].to_yaml
   end
+
+  
+  # def create
+  #   @user = User.find_by(id: auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
+  #   if @user
+  #        session[:user_id] = @user.id
+  #        redirect_to user_path(current_user)
+  #   else
+  #        redirect_to root_url
+  #   end
+  # end
 
   def destroy
     reset_session
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  root "welcome#show"
+  get '/auth/:provider/callback' => 'sessions#create'
 
-  get "/auth/github", as: :github_login
+  get '/signout' => 'sessions#destroy', :as => :signout
 
-  get "/auth/github/callback", to: "sessions#create"
-  get "/logout", to: "sessions#destroy"
+  get '/signin' => 'sessions#new', :as => :signin
 
-  resources :users, only: [:index, :show]
-  resources :repos, only: [:index, :show]
-  resources :welcome, only: [:show]
+  # root "welcome#show"
+  #
+  # get "/auth/facebook", as: :facebook_login
+  #
+  # get "/auth/facebook/callback", to: "sessions#create"
+  # get "/logout", to: "sessions#destroy"
+  #
+  # resources :users, only: [:index, :show]
+  # resources :repos, only: [:index, :show]
+  # resources :welcome, only: [:show]
 end
