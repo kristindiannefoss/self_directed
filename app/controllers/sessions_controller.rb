1451677a87976c98e761b59327953bc3cecb5216
Kris Foss <kris.foss@gmail.com>
2016-06-07 23:45:58 -0600

    reviving yoda

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
   def create
-    # binding.pry
     @user = User.find_by(id: auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
     if @user
          session[:user_id] = @user.id
