66c976a4aa590da6c182d33a9322864fd97383ff
Kristin Foss <kris.foss@gmail.com>
2016-06-08 23:24:28 -0600

    Update weather_service.rb
    remove secret keys

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -5,15 +5,14 @@ class WeatherService
 
   def initialize
     @connection = Faraday.new(url: 'http://api.openweathermap.org/data/2.5')
-    # connection.params["APPID"] = "78349e07d595ad8b249034c66e4e2303"
     # connection.params["APPID"] = ENV["WEATHER_KEY"]
     # @current_user = current_user
   end
 
   def info(city_name)
     binding.pry
-    # parse(connection.get("weather", ["q=#{city_name}", "APPID=78349e07d595ad8b249034c66e4e2303"]))
-    parse(connection.get("/weather", {q: city_name, APPID: "78349e07d595ad8b249034c66e4e2303"}))
+    # parse(connection.get("weather", ["q=#{city_name}", "APPID="]))
+    parse(connection.get("/weather", {q: city_name, APPID: ""}))
 # conn.get '/nigiri', { :name => 'Maguro' }   # GET http://sushi.com/nigiri?name=Maguro
   end
 
