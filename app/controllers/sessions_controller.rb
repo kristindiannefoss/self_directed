bb3aa6b8bb5dbee35d8f59337d23eef4a5dc3ee4
Kris Foss <kris.foss@gmail.com>
2016-06-13 11:30:36 -0600

    development is working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by(id: auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
+    @user = User.find_by(uid: auth_hash[:uid]) || User.create_from_omniauth(auth_hash)
     if @user
          session[:user_id] = @user.id
-         redirect_to dashboard_path(current_user)
+         redirect_to dashboard_path(@user)
     else
          redirect_to root_url
     end
